5df0e14f86db45d7a071ed7d6bc01f60f7036936
matzryo <matzryo@gmail.com>
2018-07-11 09:46:01 +0900

    [update] テストリファクタリング

--- spec/lib/tasks_spec.rb
+++ spec/lib/tasks_spec.rb
@@ -2,48 +2,46 @@ require 'spec_helper'
 require 'tasks'
 
 describe '#fact' do
+  subject { fact(num) }
   context '0のとき' do
-    it '1を返す' do
-      expect(fact(0)).to eq(1)
-    end
+    let(:num) { 0 }
+    it { is_expected.to eq(1) }
   end
   context '負の数のとき' do
+    let(:num) { -1 }
     it 'エラーを返す' do
-      expect { fact(-1) }.to raise_error(ArgumentError)
+      expect { subject }.to raise_error(ArgumentError)
     end
   end
   context '正の数のとき' do
-    it '整数を返す' do
-      expect(fact(4)).to eq(24)
-    end
+    let(:num) { 4 }
+    it { is_expected.to eq(24) }
   end
 end
 
 describe '#my_include?' do
+  subject { ary.my_include?(elm) }
   let(:ary) { [1, '2', nil]}
   context '含むとき' do
-    it 'trueを返す' do
-      expect(ary.my_include?(1)).to eq(true)
-    end
+    let(:elm) { 1 }
+    it { is_expected.to eq(true) }
   end
   context '含まないとき' do
-    it 'trueを返す' do
-      expect(ary.my_include?(2)).to eq(false)
-    end
+    let(:elm) { 2 }
+    it { is_expected.to eq(false) }
   end
 end
 
 describe '#my_concat' do
+  subject { ary.my_concat(another) }
   let(:ary) { [1, '2', nil] }
   let(:another) { [3, '4', :five] }
-  it '順番を保持する' do
-    expect(ary.my_concat(another)).to eq(ary + another)
-  end
+  it { is_expected.to eq(ary + another) }
 end
 
 describe '#my_fetch' do
+  subject { ary.my_fetch(index) }
   let(:ary) { [1, '2', nil] }
-  it 'インデックスに相当する要素を返す' do
-    expect(ary.my_fetch(1)).to eq(ary[1])
-  end
+  let(:index) { 1 }
+  it { is_expected.to eq(ary[index]) }
 end
