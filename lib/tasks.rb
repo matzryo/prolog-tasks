be3efee67328448999152ff35240a5aab1684e78
matzryo <matzryo@gmail.com>
2018-07-16 22:23:51 +0900

    [WIP][update] refinements化
    だが、protectedメソッドが呼ばれない
    https://github.com/matzryo/mynotes/issues/3

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -1,88 +1,103 @@
-class Integer
-  def fact
-    raise RangeError, 'nは0以上の値でなければなりません' if self < 0
-    return 1 if zero?
-    self * (self - 1).fact
-  end
-end
-
-# refinementsで!メソッドを上書きする
-class Array
-  def my_include?(target_element)
-    each do |elm|
-      return true if elm == target_element
+module Factorial
+  refine Integer do
+    def fact
+      raise RangeError, 'nは0以上の値でなければなりません' if self < 0
+      return 1 if zero?
+      self * (self - 1).fact
     end
-    false
   end
 end
 
-class Array
-  def my_concat(another)
-    mycopy = inject([]) { |accum, elm| accum.push elm }
-    another.inject(mycopy) { |accum, elm| accum.push elm }
+module MyInclude
+  refine Array do
+    def my_include?(target_element)
+      each do |elm|
+        return true if elm == target_element
+      end
+      false
+    end
   end
+end
+
+module MyConcat
+  refine Array do
+    def my_concat(another)
+      mycopy = inject([]) { |accum, elm| accum.push elm }
+      another.inject(mycopy) { |accum, elm| accum.push elm }
+    end
+  end
+end
   
-  def my_fetch(index)
-    each_with_index do |elm, i|
-      return elm if i == index
+module MyFetch
+  refine Array do
+    def my_fetch(index)
+      each_with_index do |elm, i|
+        return elm if i == index
+      end
     end
   end
 end
 
-class Array
-  def my_quicksort
-    clone.my_inner_quicksort
-  end
-
-  protected
-
-  def my_inner_quicksort
-    return self if length <= 1
-
-    small, large = my_partition
-
-    small.my_quicksort + large.my_quicksort
-  end
-
-  private
-
-  def my_partition
-    left = 0
-    right = length - 1
-
-    while left < right
-      left, right = my_search(left, right)
-      break if left >= right
-      my_swap(left, right)
+module MyQuickSort1
+  refine Array do
+    def my_quicksort
+      byebug
+      # 非破壊的メソッドにしたい
+      clone.my_inner_quicksort
     end
 
-    [self[0...left], self[left..-1]]
-  end
+    protected
 
-  def pivot
-    self[(length / 2)]
-  end
+    def my_inner_quicksort
+      return self if length <= 1
 
-  def my_search(left, right)
-    left += 1 while self[left] < pivot
-    right -= 1 while self[right] > pivot
-    [left, right]
-  end
+      small, large = my_partition
 
-  def my_swap(one, another)
-    tmp = self[one]
-    self[one] = self[another]
-    self[another] = tmp
+      small.my_quicksort + large.my_quicksort
+    end
+
+    private
+
+    def my_partition
+      left = 0
+      right = length - 1
+
+      while left < right
+        left, right = my_search(left, right)
+        break if left >= right
+        my_swap(left, right)
+      end
+
+      [self[0...left], self[left..-1]]
+    end
+
+    def pivot
+      self[(length / 2)]
+    end
+
+    def my_search(left, right)
+      left += 1 while self[left] < pivot
+      right -= 1 while self[right] > pivot
+      [left, right]
+    end
+
+    def my_swap(one, another)
+      tmp = self[one]
+      self[one] = self[another]
+      self[another] = tmp
+    end
   end
 end
 
-class Array
-  def my_quicksort2
-    return self if length <= 1
-    # lastだと、無限再帰になりうる
-    pivot = pop
-    smaller, larger = partition { |e| e < pivot }
-    push pivot
-    smaller.my_quicksort2.append + [pivot] + larger.my_quicksort2
+module MyQuickSort2
+  refine Array do
+    def my_quicksort2
+      return self if length <= 1
+      # lastだと、無限再帰になりうる
+      pivot = pop
+      smaller, larger = partition { |e| e < pivot }
+      push pivot
+      smaller.my_quicksort2.append + [pivot] + larger.my_quicksort2
+    end
   end
 end
--- spec/lib/tasks_spec.rb
+++ spec/lib/tasks_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'tasks'
 
 describe '#fact' do
+  using Factorial
   subject { num.fact }
   context '0のとき' do
     let(:num) { 0 }
@@ -20,6 +21,7 @@ describe '#fact' do
 end
 
 describe '#my_include?' do
+  using MyInclude
   subject { ary.my_include?(elm) }
   let(:ary) { [1, '2', nil]}
   context '含むとき' do
@@ -33,6 +35,7 @@ describe '#my_include?' do
 end
 
 describe '#my_concat' do
+  using MyConcat
   subject { ary.my_concat(another) }
   let(:ary) { [1, '2', nil] }
   let(:another) { [3, '4', :five] }
@@ -40,6 +43,7 @@ describe '#my_concat' do
 end
 
 describe '#my_fetch' do
+  using MyFetch
   subject { ary.my_fetch(index) }
   let(:ary) { [1, '2', nil] }
   let(:index) { 1 }
@@ -47,6 +51,7 @@ describe '#my_fetch' do
 end
 
 describe '#my_quicksort' do
+  using MyQuickSort1
   subject { ary.my_quicksort }
   let(:ary) { [1, 7, 2, 4, 10] }
   it { is_expected.to eq(ary.sort) }
@@ -58,6 +63,7 @@ describe '#my_quicksort' do
 end
 
 describe '#my_quicksort2' do
+  using MyQuickSort2
   subject { ary.my_quicksort2 }
   let(:ary) { [1, 7, 2, 4, 10] }
   it { is_expected.to eq(ary.sort) }
